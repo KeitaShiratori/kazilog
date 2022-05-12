4996114c2f55cb8ee75510983cfe47052505a9b4
KeitaShiratori <keita.shiratori@gmail.com>
2022-05-13 05:22:13 +0900

    add mutation addCategory

--- api/mutations/index.ts
+++ api/mutations/index.ts
@@ -1,9 +1,11 @@
 import { doneKazi } from './doneKazi'
 import { updateTimeline } from './updateTimeline'
 import { addKazi } from './addKazi'
+import { addCategory } from './addCategory'
 
 export default {
   doneKazi,
   updateTimeline,
   addKazi,
+  addCategory,
 }
